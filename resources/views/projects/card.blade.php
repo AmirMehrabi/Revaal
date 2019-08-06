3d09b7d9c4b93723b3a97218bcafbec86e0d9a85
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-06 23:10:59 +0430

    Fixed a minor issue with project card grid

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,4 +1,3 @@
-        <div class="lg:w-1/3 px-3 pb-6">
             <div class="card" style="height: 200px;">
                 <h3 class="text-xl py-4 -mr-5 mb-3 border-r-4 border-blue-300 pr-4">
                     <a href="{{ $project->path()}}">{{$project->title}}</a>
@@ -6,4 +5,3 @@
 
                 <div class="text-gray-600">{{str_limit($project->description)}}</div>
             </div>
-        </div>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -13,7 +13,9 @@
 
 <div class="lg:flex lg:flex-wrap -mx-3">
     @forelse ($projects as $project)
-        @include('projects.card')
+        <div class="lg:w-1/3 px-3 pb-6">
+            @include('projects.card')   
+        </div>
     @empty
         <div>پروژه‌ای وجود ندارد</div>
     @endforelse
