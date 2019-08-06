6e0aa13ea99635ec8bd6f813ac439f96e369938e
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-06 23:08:06 +0430

    Extract the project card

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -13,15 +13,7 @@
 
 <div class="lg:flex lg:flex-wrap -mx-3">
     @forelse ($projects as $project)
-        <div class="lg:w-1/3 px-3 pb-6">
-            <div class="card" style="height: 200px;">
-                <h3 class="text-xl py-4 -mr-5 mb-3 border-r-4 border-blue-300 pr-4">
-                    <a href="{{ $project->path()}}">{{$project->title}}</a>
-                </h3>
-
-                <div class="text-gray-600">{{str_limit($project->description)}}</div>
-            </div>
-        </div>
+        @include('projects.card')
     @empty
         <div>پروژه‌ای وجود ندارد</div>
     @endforelse
