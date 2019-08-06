2e6fefa3537399698f7938856bb4a89e317d7475
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-06 23:07:46 +0430

    Worked on project view

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -14,7 +14,7 @@
 <div class="lg:flex lg:flex-wrap -mx-3">
     @forelse ($projects as $project)
         <div class="lg:w-1/3 px-3 pb-6">
-            <div class="bg-white ml-4 rounded-lg shadow p-5" style="height: 200px;">
+            <div class="card" style="height: 200px;">
                 <h3 class="text-xl py-4 -mr-5 mb-3 border-r-4 border-blue-300 pr-4">
                     <a href="{{ $project->path()}}">{{$project->title}}</a>
                 </h3>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,9 +3,42 @@
 
 @section('content')
 
-<h1>{{$project->title}}</h1>
 
-<div>
-    {{$project->descriotion}}
-</div>
+<header class="flex items-center mb-4 py-4">
+    <div class="flex justify-between items-center w-full ">
+        <h3>پروژه‌های من</h3>
+        <a href="/projects/create" class="button">پروژه‌ی جدید</a>
+    </div>
+
+</header>
+
+<main>
+    <div class="lg:flex -mx-3">
+        <div class="lg:w-3/4 px-3 mb-6">
+            <div class="mb-6">
+                <h2 class="text-gray-600 mb-3">کارها</h2>
+                <div class="card">لورم ایپسوم</div>
+
+
+            </div>
+
+            <div class="mb-6">
+                <h2 class="text-gray-600 mb-3">یادداشت‌های عمومی</h2>
+
+                <div class="card">لورم ایپسوم</div>
+            </div>
+
+        </div>
+
+        <div class="lg:w-1/4 px-3">
+            <div class="card">
+                <h1>{{$project->title}}</h1>
+
+                <div>{{$project->description}}</div>
+
+                <a href="/projects">بازگشت</a>
+            </div>
+        </div>
+    </div>
+</main>
 @endsection
