a5777a90abc86711940638df12d63f8ffa4752f9
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-08 03:24:36 +0430

    Added server-side validation errors visible

--- resources/views/projects/form.blade.php
+++ resources/views/projects/form.blade.php
@@ -28,3 +28,11 @@
         
 
     </div>
+
+        @if ($errors->any())
+            <div class="field mt-6">
+                @foreach ($errors->all() as $error)
+                    <li class="text-sm text-red-700">{{$error}}</li>
+                @endforeach
+            </div>            
+        @endif
