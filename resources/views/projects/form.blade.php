31b8ca6f5ba2c82b031189a2ad50ac8d2d15b187
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-08 03:25:02 +0430

    Added client side validation

--- resources/views/projects/form.blade.php
+++ resources/views/projects/form.blade.php
@@ -6,7 +6,7 @@
         <label class="lable text-sm mb-2 block" for="title">تیتر</label>
 
         <div class="control">
-            <input type="text" name="title" placeholder="پروژه‌ی جدید من" value="{{$project->title}}" class="input bg-transparent border border-grey-400 rounded p-2 text-xs w-full">
+            <input type="text" name="title" placeholder="پروژه‌ی جدید من" value="{{$project->title}}" required class="input bg-transparent border border-grey-400 rounded p-2 text-xs w-full">
         </div>
     </div>
 
@@ -15,7 +15,7 @@
         <label class="lable text-sm mb-2 block" for="title">تیتر</label>
 
         <div class="control">
-            <textarea name="description" placeholder="باید برنامه نویسی یاد بگیرم" class="input bg-transparent border border-grey-400 rounded p-2 text-xs w-full">{{$project->description}}</textarea>
+            <textarea name="description" placeholder="باید برنامه نویسی یاد بگیرم" class="input bg-transparent border border-grey-400 rounded p-2 text-xs w-full" required>{{$project->description}}</textarea>
         </div>
     </div>
 
@@ -23,7 +23,6 @@
         <div class="control">
             <a href="{{$project->path()}}">انصراف</a>
             <button type="submit" class="button text-center">{{$buttonText}}</button>
-
         </div>
         
 
