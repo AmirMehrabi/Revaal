0b82a865ea1bed2def5f350bf7f1359e63c0eebe
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-08 15:04:54 +0430

    Created a method for update

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 \App\Project::created(function ($project) {
     \App\Activity::create([
-        'project_id' => $project->id
+        'project_id' => $project->id,
+        'description' => 'created'
+    ]);
+});
+
+
+\App\Project::created(function ($project) {
+    \App\Activity::create([
+        'project_id' => $project->id,
+        'description' => 'created'
     ]);
 });
 /*
