2974b78f07f38f23759f076701ea0a4b2dbe4a61
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-09 23:13:23 +0430

    Some minor refactors

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -74,6 +74,26 @@
             @include('projects.card')
 
             @include('projects.activity.card')
+
+
+            <div class="card flex flex-col" style="height: 200px;">
+                <h3 class="text-xl py-4 -mr-5 mb-3 border-r-4 border-blue-300 pr-4">
+                    <a href="{{ $project->path()}}">افزودن کاربر</a>
+                </h3>
+
+                <footer class=" flex-1">
+                    <form method="POST" action="{{$project->path() . '/invitations'}}">
+                        @csrf
+
+                        <div class="mb-2">
+                            <input type="email" name="email" class="border border-grey-500 rounded w-full px-3 py-2" placeholder="آدرس ایمیل">
+                        </div>
+
+                        <button type="submit" class="button">دعوت</button>
+                    </form>
+                </footer>
+            </div>
+
         </div>
     </div>
 </main>
