b913fda2f55b75ba0b8b2558c4994ceac2c715fd
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-09 16:35:25 +0430

    Refactor some activity methods

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -5,13 +5,37 @@ namespace App;
 trait RecordsActivity
 {
 
-    public $old = [];
+    public $oldAttributes = [];
 
     public static function bootRecordsActivity()
     {
         static::updating(function ($model) {
-            $model->old = $model->getOriginal();
+            $model->oldAttributes = $model->getOriginal();
         });
+
+
+
+
+        foreach (self::recordableEvents() as $event) {
+            static::$event(function ($model) use ($event) {
+
+                if (class_basename($model) !== 'Project') {
+                    $event = "{$event}_" . strtolower(class_basename($model));
+                }
+
+                $model->recordActivity($event);
+            });
+        }
+    }
+
+    protected static function recordableEvents()
+    {
+        if (isset(static::$recordableEvents)) {
+            $recordableEvents = static::$recordableEvents;
+        } else {
+            $recordableEvents = ['created', 'updated', 'deleted'];
+        }
+        return $recordableEvents;
     }
 
     public function recordActivity($description)
@@ -33,7 +57,7 @@ trait RecordsActivity
     {
         if ($this->wasChanged()) {
             return [
-                'before' => array_except(array_diff($this->old, $this->getAttributes()), 'updated_at'),
+                'before' => array_except(array_diff($this->oldAttributes, $this->getAttributes()), 'updated_at'),
                 'after' => array_except($this->getChanges(), 'updated_at')
             ];
         }
