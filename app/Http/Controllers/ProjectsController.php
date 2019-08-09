a797adeb62bd9feda57c56fbc443278e5966818d
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-09 17:37:03 +0430

    Added authorization for deleting projects

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -47,6 +47,7 @@ class ProjectsController extends Controller
 
     public function destroy(Project $project)
     {
+        $this->authorize('update', $project);
         $project->delete();
         return redirect('/projects');
     }
