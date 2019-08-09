3f71f3cd753b1eb9fc1b141a8d8286c92ffc2884
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-09 22:20:08 +0430

    We have a method to invite users

--- app/Http/Controllers/ProjectInvitationsController.php
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ProjectInvitationsController extends Controller
 {
-    //
+
+
+    public function store(Project $project)
+    {
+
+        $this->authorize('update', $project);
+
+        request()->validate([
+            'email' => 'exists:users,email'
+        ], [
+            'email.exists' => 'کاربری که دعوت می‌کنید باید عضو روال باشد'
+        ]);
+
+        $user = User::whereEmail(request('email'))->first();
+
+        $project->invite($user);
+
+        return redirect($project->path());
+    }
 }
