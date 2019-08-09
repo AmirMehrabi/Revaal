e6f82e43e8fae522753181a9ba3eb752fa40d1cc
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-10 03:10:32 +0430

    Cutted the whole new project modal

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -24,34 +24,11 @@
     @endforelse
 </main>
 
-<modal name="hello-world" classes="p-10 bg-white rounded-lg shadow mr-auto" height="auto">
-  <h1 class="text-center text-xl my-4">یک پروژه‌ی جدید شروع کنید</h1>
-
-  <div class="flex">
-      <div class="flex-1 ml-4">
-          <div class="mb-4">
-              <label for="title" class="text-sm block mb-2">تیتر</label>
-              <input type="text" name="title" id="title" class="border border-gray-300 p-2 text-sm block w-full rounded" placeholder="پروژه‌ی جدید من">
-          </div>
-
-          <div class="mb-4">
-              <label for="description" class="text-sm block mb-2">تیتر</label>
-              <textarea name="description" id="description" class="border border-gray-300 p-2 text-sm block w-full rounded" placeholder="پروژه‌ی جدید من" rows="7"> </textarea>
-          </div>
-      </div>
-
-      <div class="flex-1 mr-4">
-          <div class="mb-4">
-              <label class="text-sm block mb-2">کارهای مربوط به این پروژه</label>
-              <input type="text" class="border border-gray-300 p-2 text-sm block w-full rounded" placeholder="کار اول">
-          </div>
-      </div>
-  </div>
-</modal>
+<new-project-modal></new-project-modal>
 
 
 
-<a href="#" @click.prevent="$modal.show('hello-world')">نمایش مودال</a>
+<a href="#" @click.prevent="$modal.show('new-project')">نمایش مودال</a>
 
 
 @endsection
