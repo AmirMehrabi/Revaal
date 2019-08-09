3e3641548b20d5c5cc966abcf897cf6873b3c289
Amir Mehrabi <aut0run2011@gmail.com>
2019-08-09 22:54:59 +0430

    Cleaned some code

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,9 +1,9 @@
-            <div class="card" style="height: 200px;">
+            <div class="card flex flex-col" style="height: 200px;">
                 <h3 class="text-xl py-4 -mr-5 mb-3 border-r-4 border-blue-300 pr-4">
                     <a href="{{ $project->path()}}">{{$project->title}}</a>
                 </h3>
 
-                <div class="text-gray-600 mb-4">{{str_limit($project->description)}}</div>
+                <div class="text-gray-600 mb-4 flex-1">{{str_limit($project->description)}}</div>
 
                 <footer>
                     <form method="POST" action="{{$project->path()}}" class="text-left">
